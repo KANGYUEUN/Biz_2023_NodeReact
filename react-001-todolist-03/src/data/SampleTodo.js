0eb18276781395094879cc789837f94a9826ddae
KANGYUEUN <yu1128eun@kakao.com>
2023-08-04 09:21:03 +0900

    0804

---
+++ react-001-todolist-03/src/data/SampleTodo.js
@@ -0,0 +1,39 @@
+const SampleData = [
+  {
+    id: 0,
+    sdate: "2023-08-03",
+    stime: "10:00:00",
+    content: "React 학습하기",
+    complete: true,
+  },
+  {
+    id: 1,
+    sdate: "2023-08-03",
+    stime: "10:00:10",
+    content: "TodoList",
+    complete: true,
+  },
+  {
+    id: 2,
+    sdate: "2023-08-03",
+    stime: "10:00:20",
+    content: "React 재미지다",
+    complete: false,
+  },
+  {
+    id: 3,
+    sdate: "2023-08-03",
+    stime: "10:00:30",
+    content: "내일은 약속티비",
+    complete: false,
+  },
+  {
+    id: 4,
+    sdate: "2023-08-03",
+    stime: "10:00:40",
+    content: "배고프다",
+    complete: false,
+  },
+];
+
+export { SampleData };
