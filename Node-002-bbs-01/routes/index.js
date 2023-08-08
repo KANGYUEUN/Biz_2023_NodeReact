00d2cb9fc30501a61078913db51079da0a67181a
KANGYUEUN <yu1128eun@kakao.com>
2023-08-08 16:51:13 +0900

    0808

--- Node-002-bbs-01/routes/index.js
+++ Node-002-bbs-01/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
-  // view 폴더에 index.pug 라는 파일을 찾아서 Rendering 하고 응답하라.
+  // views 폴더에 index.pug 라는 파일을 찾아서 Rendering 하고 응답하라.
   res.render("index", { title: "callor.com Express" });
 });
 
